6e5de12f38f2c230ff0e8245532f5c0ed7a1b1e8
Ravi Pandey <pandeyravi181@gmail.com>
2021-12-28 12:33:04 +0530

    Program for char to boolean.

---
+++ CharAndBoolean/src/academy/learnprogramming/Main.java
@@ -0,0 +1,13 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char myChar = 'D';
+        System.out.println(myChar);
+
+        boolean myBoolean = true;
+        System.out.println(myBoolean);
+    }
+}
